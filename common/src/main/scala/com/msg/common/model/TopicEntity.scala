8f17d8d46f91411d8bcccd712d0809dec1e043e0
zhangbin <elviszhang2002@gmail.com>
2016-03-16 22:04:31 +0800

    adjust package dir structure

---
+++ common/src/main/scala/com/msg/common/model/TopicEntity.scala
@@ -0,0 +1,41 @@
+package com.msg.common.model
+
+import java.util.HashSet
+import scala.concurrent.forkjoin.ThreadLocalRandom
+import java.util.ArrayList
+import java.util.HashMap
+
+case class TopicInfo(topic: String, code: String, key: String, isStore: Int, storeMethod: Int, sendNum: Int, storeNum: Int, isRegx: Int, status: Int, isTemplate: Int, swapType: Int, regMethod: String, defaultTemp: String, defaultNotify: String,broadStatus:Int)
+
+object TopicEntity {
+
+    private val topicInfoMap = new HashMap[String, TopicInfo]
+
+    def addTopicInfo(topic: String, topicInfo: TopicInfo) = {
+        topicInfoMap.put(topic, topicInfo)
+    }
+
+    def getTopicInfo(topic: String): TopicInfo = {
+        topicInfoMap.get(topic)
+    }
+
+    def removeTopicInfo(topic: String) = {
+        topicInfoMap.remove(topic)
+    }
+    def containsTopic(topic: String): Boolean = {
+        topicInfoMap.keySet().contains(topic)
+    }
+    def getTopicSendNum(topic: String): Int = {
+        if (topicInfoMap.keySet().contains(topic)) {
+            topicInfoMap.get(topic).sendNum
+        } else {
+            0
+        }
+    }
+    def removeAll = {
+        topicInfoMap.clear()
+    }
+    def getAllTopics: HashMap[String, TopicInfo] = {
+        topicInfoMap
+    }
+}
